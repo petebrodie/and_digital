d6bfcac37be66f8b28cccbc0e0e094f29e18972d
Pete <peterwbrodie@gmail.com>
2021-09-30 17:32:47 +0100

    master theorem and pseudocode

--- SolutionAND.js
+++ SolutionAND.js
@@ -6,6 +6,10 @@ function solution (input) {
 
   let results = []
   for (let i = 0; i < input.length; i++) {
+
+    // Create a subproblems of string, each having size n/b
+    // Call procedure p recursively on each subproblem
+    // Combine the results from the subproblems
     
   }
 
