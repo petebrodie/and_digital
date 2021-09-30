781cca93107847e84871de004ffcbad69a10c7fd
Pete <peterwbrodie@gmail.com>
2021-09-30 17:27:06 +0100

    initial commit

---
+++ SolutionAND.js
@@ -0,0 +1,14 @@
+/**
+* The following is the function where the solution shall be written
+*/
+
+function solution (input) {
+
+  // logic here
+
+  return null; 
+}
+
+// some example inputs
+console.log(solution('326')); // expected ouput 632,623,362,326,263,236
+console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
