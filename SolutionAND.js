49b6e351ba67003ba893187451378922b07f26a9
Pete <peterwbrodie@gmail.com>
2021-10-05 14:53:30 +0100

    data solving complete

--- SolutionAND.js
+++ SolutionAND.js
@@ -5,27 +5,28 @@
 function solution (input) {
 
   let legalCharacters = []
-  for (index in input) {
-    if (Number(input[index])) {
+  for (let index in input) {
+    if (Number(input[index]) && parseInt(input[index]) > -1) {
       legalCharacters.push(input[index])
     }
   }
 
-  // for (let i = 0; i < input.length; i++) {
-  //   let char = input[i]
-  //   let remainingChars = input.slice(0, i) + input.slice(i + 1, input.length)
+  
 
-  //   for (let permutation of results (remainingChars)) {
-  //     results.push(char + permutation)}
-  // }
-
-  return legalCharacters; 
+  
+  if (legalCharacters.length > 0) {
+    return legalCharacters.join("")
+  }
+  else {
+    console.log("No numbers were found!")
+  } 
 }
 
 
 // // some example inputs
 console.log(solution('326')); // expected ouput 632,623,362,326,263,236
 console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
+console.log(solution('ABC')); // expected ouput 632,623,362,326,263,236
 
 
 
